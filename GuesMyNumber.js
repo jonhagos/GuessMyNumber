65c17c7444e08bc154ef5eb7ee31176f6ea1153f
jonhagos <jon4geo@gmail.com>
2025-04-15 22:37:58 +0200

    DOM manuplation

---
+++ GuesMyNumber.js
@@ -0,0 +1,9 @@
+'use strict';
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = ' correct number !';
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.number').textContent = 15;
+document.querySelector('.score').textContent = 10
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
